bad224a945869d898be67678b4304a8ae152d2d7
genuinelyJin <andabin8@gmail.com>
2021-04-06 23:14:18 +0900

    hw1 merge1

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-	printf("branch test1");
+<<<<<<< HEAD
+	printf("branch master");
+=======
+	printf("branch testbranch");
+>>>>>>>testbranch
+=======
+	printf("branch testbranch2");
+>>>>>>>testbranch2
 	return 0;
 }
